482b3197a5de466812dd7f365870331a9e6bb882
John Rogers <john.clinton.rogers@gmail.com>
2023-02-15 13:08:12 -0600

    Scaffold spec file

---
+++ smartcar-endpoint-explorer/cypress/e2e/EndpointExplorer.cy.ts
@@ -0,0 +1,341 @@
+describe(`EndpointExplorer`, () => {
+  describe(`Display`, () => {
+    it(`Displays GET endpoint configurations correctly`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log(
+        "Expect the endpoint explorer to display the configuration correctly"
+      );
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the title to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the url to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the method to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect each body field to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the email to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the full name to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the phone to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Displays PUT endpoint configurations correctly`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log(
+        "Expect the endpoint explorer to display the configuration correctly"
+      );
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the title to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the url to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the method to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect each body field to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the email to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the full name to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the phone to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Displays DELETE endpoint configurations correctly`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log(
+        "Expect the endpoint explorer to display the configuration correctly"
+      );
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the title to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the url to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the method to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect each body field to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the email to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the full name to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the phone to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Displays POST endpoint configurations correctly`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log(
+        "Expect the endpoint explorer to display the configuration correctly"
+      );
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the title to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the url to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the method to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect each body field to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the email to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the full name to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the phone to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+  });
+  describe(`Validation`, () => {
+    it(`Requires valid emails`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Enter invalid value");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect validation to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the correct validation message to appear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the submission button to be disabled");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect validation to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the correct validation message to appear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the submission button to be disabled");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Enter a valid value");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect validation to disappear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the correct validation message to appear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the submission button to be enabled");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Requires valid full names`, () => {});
+    cy.log("Load page");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Enter invalid value");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect validation to display correctly");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect the correct validation message to appear");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect the submission button to be disabled");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect validation to display correctly");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect the correct validation message to appear");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect the submission button to be disabled");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Enter a valid value");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect validation to disappear");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect the correct validation message to appear");
+    cy.contains("break", { timeout: 0 });
+
+    cy.log("Expect the submission button to be enabled");
+    cy.contains("break", { timeout: 0 });
+
+    it(`Requires valid phone numbers`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Enter invalid value");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect validation to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the correct validation message to appear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the submission button to be disabled");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect validation to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the correct validation message to appear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the submission button to be disabled");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Enter a valid value");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect validation to disappear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the correct validation message to appear");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the submission button to be enabled");
+      cy.contains("break", { timeout: 0 });
+    });
+  });
+  describe(`Submission`, () => {
+    it(`Can reset the form`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Complete form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Click to reset the form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect each body field to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the email to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the full name to display correctly");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the phone to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Can submit new GET requests`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Complete form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Submit form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the request to be correct");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the response to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Can submit new PUT requests`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Complete form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Submit form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the request to be correct");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the response to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Can submit new DELETE requests`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Complete form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Submit form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the request to be correct");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the response to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Can submit new POST requests`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Complete form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Submit form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the request to be correct");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the response to display correctly");
+      cy.contains("break", { timeout: 0 });
+    });
+    it(`Handles submission errors`, () => {
+      cy.log("Load page");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Complete form");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Submit form and respond with an error");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the request to be correct");
+      cy.contains("break", { timeout: 0 });
+
+      cy.log("Expect the message to be correct");
+      cy.contains("break", { timeout: 0 });
+    });
+  });
+});
